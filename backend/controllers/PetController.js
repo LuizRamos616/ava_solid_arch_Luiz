0cada6f34355a7829bd8abddd89766503f5e7356
LuizRamos616 <luc.ricardo.ramos@gmail.com>
2026-05-11 16:08:35 -0300

    Criação do getPetById e removePetById para remover e buscar o pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -2,32 +2,32 @@ const Pet = require('../models/Pet')
 const getUserByToken = require('../helpers/get-user-by-token')
 const getToken = require('../helpers/get-tokens')
 
-module.exports = class PetController{
+module.exports = class PetController {
 
-    static async create(req, res){
-        const {name, age, weight, color} = req.body
+    static async create(req, res) {
+        const { name, age, weight, color } = req.body
         const images = req.files
         const token = getToken(req)
         const user = await getUserByToken(token)
 
-        if(!name){
-            res.status(422).json({message:'O nome é obrigatório'})
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
             return
         }
-        if(!age){
-            res.status(422).json({message:'A idade é obrigatória'})
+        if (!age) {
+            res.status(422).json({ message: 'A idade é obrigatória' })
             return
         }
-        if(!weight){
-            res.status(422).json({message:'O peso é obrigatório'})
+        if (!weight) {
+            res.status(422).json({ message: 'O peso é obrigatório' })
             return
         }
-        if(!color){
-            res.status(422).json({message:'A cor é obrigatoria'})
-            return    
+        if (!color) {
+            res.status(422).json({ message: 'A cor é obrigatoria' })
+            return
         }
-        if(!images || images.length === 0){
-            res.status(422).json({message:'A imagem é obrigatória'})
+        if (!images || images.length === 0) {
+            res.status(422).json({ message: 'A imagem é obrigatória' })
             return
         }
 
@@ -38,24 +38,102 @@ module.exports = class PetController{
             age,
             weight,
             color,
-            images:imageNames,
+            images: imageNames,
             available: true,
-            user:{
+            user: {
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 phone: user.phone
             }
-        
+
         })
-        try{
+        try {
             const newPet = await pet.save()
             res.status(201).json({
-                message:'Pet cadastrado com sucesso!',
+                message: 'Pet cadastrado com sucesso!',
                 pet: newPet
             })
-        }catch (error){
-            res.status(500).json({message: error})
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
+    static async getAll(req,res) {
+        const pets = await Pet.find().sort('-createdAt')
+
+        res.status(200).json({
+            succes: true,
+            count: pets.length,
+            data: pets,
+        })
+        return
+    }
+    static async getAllUserPets(req, res) {
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        try {
+            const pets = await Pet.find({ 'user._id': user_id }).sort('createdAt')
+            res.status(200).json({ pets })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
+    static async getAllUserAdoptions(req, res) {
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'adopter._id': user_id }).sort('-createdAt')
+        
+        res.status(200).json({ 
+            succes: true,
+            count: pets.length,
+            data: pets,
+         })
+         return
+        
+    }
+    static async getPetById(req,res){
+        const id = req.params.id
+
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(422).json({message: 'O id o pet é obrigatório'})
+            return
+        }
+        try{
+            const pet = await Pet.findById(id)
+            if(!pet) {
+                res.status(404).json({ message: 'Pet não encontrado' })
+                return
+            }
+        res.status(200).json({ success: true, data: pet })
+        }catch (error) {
+            res.status(503).json({message: error})
+        }
+        const pet = await Pet.findById(id)
+        if (!pet){
+            res.status(404).json({ massage: 'Pet não encontrado '})
+            return
+        }
+        res.status(200).json({data: pet})
+    }
+    static async removePetById(req, res) {
+        const {id} = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            res.status(422).json({message: 'o id do pet é obrigatorio'})
+            return
+        }
+        const pet = await Pet.findById(id)
+        if(!pet) {
+            res.status(404).json({message: 'Pet não encontrado'})
+            return
+        }
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if(pet.user._id.toString() !== user._id.toString()) {
+            res.status(403).json({message: 'Pet removido com sucesso!', data: pet})
+            
         }
     }
 }
