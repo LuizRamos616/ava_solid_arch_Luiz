e007fcedd0c794b10f07186dd9a4ae866aaed80b
LuizRamos616 <luc.ricardo.ramos@gmail.com>
2026-05-11 14:27:23 -0300

    Implementação do getAllUserPets filtrando pelo user._id
    Implementação do getAllUserAdoptions diltrando por adopter._id
    
    Ordenação dos resultados do mais recente para o mais antigo

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -2,32 +2,32 @@ const Pet = require('../models/Pet')
 const getUserByToken = require('../helpers/get-user-by-token')
 const getToken = require('../helpers/get-tokens')
 
-module.exports = class PetController{
+module.exports = class PetController {
 
-    static async create(req, res){
-        const {name, age, weight, color} = req.body
+    static async create(req, res) {
+        const { name, age, weight, color } = req.body
         const images = req.files
         const token = getToken(req)
         const user = await getUserByToken(token)
 
-        if(!name){
-            res.status(422).json({message:'O nome é obrigatório'})
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
             return
         }
-        if(!age){
-            res.status(422).json({message:'A idade é obrigatória'})
+        if (!age) {
+            res.status(422).json({ message: 'A idade é obrigatória' })
             return
         }
-        if(!weight){
-            res.status(422).json({message:'O peso é obrigatório'})
+        if (!weight) {
+            res.status(422).json({ message: 'O peso é obrigatório' })
             return
         }
-        if(!color){
-            res.status(422).json({message:'A cor é obrigatoria'})
-            return    
+        if (!color) {
+            res.status(422).json({ message: 'A cor é obrigatoria' })
+            return
         }
-        if(!images || images.length === 0){
-            res.status(422).json({message:'A imagem é obrigatória'})
+        if (!images || images.length === 0) {
+            res.status(422).json({ message: 'A imagem é obrigatória' })
             return
         }
 
@@ -38,24 +38,58 @@ module.exports = class PetController{
             age,
             weight,
             color,
-            images:imageNames,
+            images: imageNames,
             available: true,
-            user:{
+            user: {
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 phone: user.phone
             }
-        
+
         })
-        try{
+        try {
             const newPet = await pet.save()
             res.status(201).json({
-                message:'Pet cadastrado com sucesso!',
+                message: 'Pet cadastrado com sucesso!',
                 pet: newPet
             })
-        }catch (error){
-            res.status(500).json({message: error})
+        } catch (error) {
+            res.status(500).json({ message: error })
         }
+    }
+    static async getAll(req,res) {
+        const pets = await Pet.find().sort('-createdAt')
+
+        res.status(200).json({
+            succes: true,
+            count: pets.length,
+            data: pets,
+        })
+        return
+    }
+    static async getAllUserPets(req, res) {
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        try {
+            const pets = await Pet.find({ 'user._id': user_id }).sort('createdAt')
+            res.status(200).json({ pets })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
+    static async getAllUserAdoptions(req, res) {
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'adopter._id': user_id }).sort('-createdAt')
+        
+        res.status(200).json({ 
+            succes: true,
+            count: pets.length,
+            data: pets,
+         })
+         return
+        
     }
 }
